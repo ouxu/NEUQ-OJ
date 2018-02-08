dd38d9aeeeb57933fd88bba5ef1768f25a1d784d
xujiajun01 <xujiajun01@baidu.com>
2018-02-08 23:16:27 +0800

    修改部署方式，添加错误监控

--- app/src/api/index.js
+++ app/src/api/index.js
@@ -3,7 +3,7 @@
  */
 
 // const __APIHOST__ = 'http://192.168.1.189:8080'
-const __APIHOST__ = 'http://112.126.70.205:8088'
+const __APIHOST__ = 'http://140.143.222.61:8088'
 const __DEVHOST__ = 'http://192.168.1.18:3000'
 const apiMaker = path => `${__APIHOST__}/${path}`
 const devApiMaker = path => `${__DEVHOST__}/${path}`
